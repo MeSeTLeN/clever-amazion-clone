3351bbe201f97b78239fc7fee29341f784c70714
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-21 15:26:42 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Amazion Store </h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCf0-k52bbEU-njFfjZnfOcgsartq2KBxc',
+  authDomain: 'clever-react--clone.firebaseapp.com',
+  projectId: 'clever-react--clone',
+  storageBucket: 'clever-react--clone.appspot.com',
+  messagingSenderId: '488414516749',
+  appId: '1:488414516749:web:ba38406008e100f9162744',
+  measurementId: 'G-0MQCVMPD56',
+}
