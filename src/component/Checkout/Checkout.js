c1f39fedd88f69388a51a9c143d6b3c0a47777f5
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-27 11:35:21 +0200

    feat: selector getBasketTotal

--- src/component/Checkout/Checkout.js
+++ src/component/Checkout/Checkout.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Checkout.css'
-import adBanner from '../../assets/img_desktop/Checkout/adBanner.jpg'
 import Subtotal from '../Subtotal/Subtotal'
 
 function Checkout() {
   return (
     <div className='checkout'>
       <div className='checkout__left'>
-        <img className='checkout__ad' src={adBanner} alt='banner' />
         <div>
           <h2 className='checkout__title'>Shopping Cart</h2>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCf0-k52bbEU-njFfjZnfOcgsartq2KBxc',
-  authDomain: 'clever-react--clone.firebaseapp.com',
-  projectId: 'clever-react--clone',
-  storageBucket: 'clever-react--clone.appspot.com',
-  messagingSenderId: '488414516749',
-  appId: '1:488414516749:web:ba38406008e100f9162744',
-  measurementId: 'G-0MQCVMPD56',
+  apiKey: 'AIzaSyB6DdFS14P_4HxCzVQ--Uz3YRiwQ2f3jj8',
+  authDomain: 'clone-b94c2.firebaseapp.com',
+  projectId: 'clone-b94c2',
+  storageBucket: 'clone-b94c2.appspot.com',
+  messagingSenderId: '788731630010',
+  appId: '1:788731630010:web:c1d3c366965f7b4e660d83',
+  measurementId: 'G-3LG7VZNF28',
 }
